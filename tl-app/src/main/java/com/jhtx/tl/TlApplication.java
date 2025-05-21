4bf6186090d4c4b72f1580c42e5cdc7789e3d9f0
1045629439@qq.com <1045629439@qq.com>
2025-05-21 09:54:12 +0800

    项目初始化

--- tl-app/src/main/java/com/jhtx/tl/TlApplication.java
+++ tl-app/src/main/java/com/jhtx/tl/TlApplication.java
@@ -2,11 +2,13 @@ package com.jhtx.tl;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @Slf4j
+@MapperScan("com.jhtx.tl.infra.repository.mapper")
 @SpringBootApplication
 public class TlApplication {
 
